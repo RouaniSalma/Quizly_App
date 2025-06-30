d9730da866a1fc15869de97af9040c4b8e34aaf9
Salma Rouani <salmarouani305@gmail.com>
2025-06-30 21:49:46 +0100

    debug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+%n%23vjt71f!+7x9r8ft)=juzl1h2_&7z&bt)*dr_96dfp0wo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
